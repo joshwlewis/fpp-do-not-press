f71bd4a9bc2eb135094d5525b780af070b4378e2
Josh W Lewis <josh.w.lewis@gmail.com>
2024-12-20 11:25:06 -0600

    Add up / down commands

--- commands/down.sh
+++ commands/down.sh
@@ -1,3 +1,51 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "On Script" 
+# do-not-press-down.sh runs when the physical DO NOT PRESS button is depressed.
+# This is designed to provide feedback as to whether or not the button release
+# will activate a sequence. If the $TARGET_PLAYLIST is not already playing, this runs
+# an ok sound + effect. If $TARGET_PLAYLIST is already playing (somebody is spamming
+# the button), this runs an err sound + effect. Either way, the sound + effect
+# will play in the background without interrupting anything already playing.
+# For this reason, use short sounds and sequences with this script (under 1
+# second).
+
+shopt -s nullglob
+
+PRIMARY_PLAYLIST="show"
+TARGET_PLAYLIST="do-not-press"
+REMOTE_PLAYLIST="remote"
+TMPDIR=/var/tmp
+DOWN_FILE="$TMPDIR/$TARGET_PLAYLIST-down.txt"
+INT_FILE="$TMPDIR/$PRIMARY_PLAYLIST-interrupt.txt"
+CURRENT_PLAYLIST=$(fpp -s | cut -d ',' -f 4)
+NEXT_INT_AT=$(<"$INT_FILE")
+DOWN_AT=$(date +%s.%N)
+echo -n "$DOWN_AT" > "$DOWN_FILE" &
+
+if [[ $CURRENT_PLAYLIST == "$TARGET_PLAYLIST"  ||
+      $CURRENT_PLAYLIST == "$REMOTE_PLAYLIST" ||
+    ( $CURRENT_PLAYLIST == "$PRIMARY_PLAYLIST" && $(echo "$DOWN_AT < $NEXT_INT_AT" | bc -l ) -eq 1 ) ]]; then
+  echo "Selecting err media."
+  EFFECTS=("$MEDIADIR/sequences/$TARGET_PLAYLIST-err"*)
+  SOUNDS=("$MEDIADIR/music/$TARGET_PLAYLIST-err"*)
+else
+  echo "Selecting ok media."
+  EFFECTS=("$MEDIADIR/sequences/$TARGET_PLAYLIST-ok"*)
+  SOUNDS=("$MEDIADIR/music/$TARGET_PLAYLIST-ok"*)
+fi
+
+COUNT=$((${#EFFECTS[@]} < ${#SOUNDS[@]} ? ${#EFFECTS[@]} : ${#SOUNDS[@]}))
+RAND_IDX=$(($RANDOM % $COUNT))
+
+SOUND=${SOUNDS[$RAND_IDX]}
+if [ -n "${SOUND}" ]; then
+  echo "Playing $SOUND"
+  fpp -C "Play Media" "$SOUND" 1 0
+fi
+
+EFFECT=${EFFECTS[$RAND_IDX]}
+EFFECT_NAME=$(basename "$EFFECT" | cut -d "." -f1)
+if [ -n "${EFFECT_NAME}" ]; then
+  echo "Playing $EFFECT_NAME"
+  fpp -C "FSEQ Effect Start" "$EFFECT_NAME" 0 1
+fi
--- commands/up.sh
+++ commands/up.sh
@@ -1,3 +1,60 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "Off Script"
+# do-not-press-up.sh runs when the physical DO NOT PRESS button is released.
+# This script is designed to trigger only one $TARGET_PLAYLIST item per push. Each
+# successive run of this script should play the next $TARGET_PLAYLIST item or cycle
+# back to the beginning.
+# If $TARGET_PLAYLIST is already playing (probably from a previous button push),
+# it won't enqueue another $TARGET_PLAYLIST item.
+# If $TARGET_PLAYLIST is running and a long press is detected, that item will be
+# stopped and  normally scheduled programming will resume.
+# This script will only interrupt the $PRIMARY_PLAYLIST every $MIN_INT_FREQUENCY.
+
+shopt -s nullglob
+
+TARGET_PLAYLIST="do-not-press"
+PRIMARY_PLAYLIST="show"
+REMOTE_PLAYLIST="remote"
+TMPDIR=/var/tmp
+LONG_PRESS_LENGTH=1500
+MIN_INT_FREQUENCY="${1:-300000}"
+DOWN_FILE="$TMPDIR/$TARGET_PLAYLIST-down.txt"
+INT_FILE="$TMPDIR/$PRIMARY_PLAYLIST-interrupt.txt"
+UP_AT=$(date +%s.%N)
+CURRENT_PLAYLIST=$(fpp -s | cut -d ',' -f 4)
+NEXT_INT_AT=$(<"$INT_FILE")
+if [[  "$CURRENT_PLAYLIST" == "$TARGET_PLAYLIST" ]]; then
+  echo "Already playing $TARGET_PLAYLIST."
+  DOWN_AT=$(<"$DOWN_FILE")
+  PRESS_LENGTH=$(echo "(${UP_AT} * 1000) - (${DOWN_AT} * 1000)" | bc | cut -f1 -d\.)
+  if (( $PRESS_LENGTH > $LONG_PRESS_LENGTH )); then
+    echo "Stopping $TARGET_PLAYLIST playlist now"
+    fpp -C "Stop Now"
+  fi
+  exit 0;
+elif [[ "$CURRENT_PLAYLIST" == "$REMOTE_PLAYLIST" ]]; then
+  echo "Not interrupting $REMOTE_PLAYLIST playlist requests"
+  exit 0;
+elif [[ "$CURRENT_PLAYLIST" == "$PRIMARY_PLAYLIST" ]]; then
+  if [[ $(echo "$UP_AT < $NEXT_INT_AT" | bc -l) -eq 1 ]]; then
+    echo "Not interrupting $PRIMARY_PLAYLIST playlist. It's happened recently."
+    exit 0;
+  fi
+fi
+
+COUNTER_FILE="$TMPDIR/$TARGET_PLAYLIST-count.txt"
+if [ -f $COUNTER_FILE ]; then
+  PLAY_COUNT=$(<$COUNTER_FILE)
+else
+  PLAY_COUNT="0"
+fi
+PLAY_COUNT=$(($PLAY_COUNT + 1))
+echo -n "$PLAY_COUNT" > $COUNTER_FILE &
+
+ITEM_COUNT=$(grep "total_items" "$MEDIADIR/playlists/$TARGET_PLAYLIST.json" | cut -d ':' -f 2 | xargs)
+IDX=$((($PLAY_COUNT % $ITEM_COUNT) + 1))
+echo "Playing $TARGET_PLAYLIST item $IDX"
+fpp -C "Insert Playlist Immediate" $TARGET_PLAYLIST $IDX $IDX 0
+
+# Store the timestamp at which the next interrupt can happen
+printf %s "$(date --date="+$((MIN_INT_FREQUENCY/1000)) seconds" +%s.%N)" > $INT_FILE &
